06e2044c09e72b13b0e38ea746a1dcbec339af06
Micqueen <105097325+Micqueen@users.noreply.github.com>
2024-06-28 14:46:57 +0800

    McQueen url.js

--- url.js
+++ url.js
@@ -8,7 +8,8 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://www.surveycake.com/s/7LkZZ',
+	'https://www.surveycake.com/s/39zyN',
+	'https://www.surveycake.com/s/XDxwM',
+	'https://www.surveycake.com/s/VaKNO'
 ];
